374a3abc555a9962609d55e2b1368e93f97e1843
Gaofei Shen <g.shen@tilburguniversity.edu>
2025-10-23 14:35:13 +0200

    improvement to plotting

--- src/plotting_results.py
+++ src/plotting_results.py
@@ -13,6 +13,20 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 RESULTS_DIR = os.path.join(PROJECT_ROOT, "results")
 
+model_layer_dict = {
+    "wav2vec2-base": 12,
+    "wav2vec2-base-960h": 12,
+    "wav2vec2-large": 24,
+    "wav2vec2-large-960h": 24,
+    "wav2vec2-large-xlsr-53": 24,
+    "hubert-base-ls960": 12,
+    "hubert-large-ll60k": 24,
+    "hubert-large-ls960-ft": 24,
+    "wavlm-base": 12,
+    "roberta-base": 12,
+    "bert-base-uncased": 12,
+    "ModernBERT-base": 22,
+}
 
 # Set up logger with time, name, level, and message
 logging.basicConfig(
@@ -26,7 +40,7 @@ logger = logging.getLogger(__name__)
 
 
 def load_result_files(results_dir: str) -> pd.DataFrame:
-    logger.info(f"Loading result files from {results_dir}...")
+    # logger.info(f"Loading result files from {results_dir}...")
     if not os.path.exists(results_dir):
         logger.error(f"Results directory {results_dir} does not exist.")
         raise FileNotFoundError(f"Results directory {results_dir} does not exist.")
@@ -35,27 +49,40 @@ def load_result_files(results_dir: str) -> pd.DataFrame:
     all_results_files = glob.glob(
         os.path.join(results_dir, "librispeech-*/**/*.csv"), recursive=True
     )
-    all_results_files = [x for x in all_results_files if "all_layers" not in x]
+    all_results_files = [
+        x for x in all_results_files if "all_layers" not in x and "_normalized" in x
+    ]
 
     all_results_df = pd.DataFrame()
     for result_file in all_results_files:
-        logger.info(f"Found results file: {result_file}")
+        # logger.info(f"Found results file: {result_file}")
         librispeech_split = result_file.split("/")[-4]
         modelname = result_file.split("/")[-3]
         probename, _, _, normalization = result_file.split("/")[-2].split("_")
         if "random" in probename:
             probename = "random_forest"
 
-        logger.info(
-            f"Parsed - Librispeech Split: {librispeech_split}, Model Name: {modelname}, Probe Name: {probename}"
-        )
+        # logger.info(
+        #     f"Parsed - Librispeech Split: {librispeech_split}, Model Name: {modelname}, Probe Name: {probename}"
+        # )
         df = pd.read_csv(result_file)
         df["modelname"] = modelname
         df["librispeech_split"] = librispeech_split
         df["probename"] = probename
         df["normalization"] = normalization
+
+        df["norm_layer"] = df["layer"] / model_layer_dict.get(modelname, 1)
         all_results_df = pd.concat([all_results_df, df], ignore_index=True)
+
     return all_results_df
+
+
+caption = """\
+Baselines:
+- Black dashed line: Random Baseline (random_baseline)
+- Black dotted line: All Feature Baseline (all_feat_baseline)
+All feature baseline is the best case results with all features intact. The feature group manipulations should be compared against these baselines.
+"""
 
 
 def plot_results(all_results_df: pd.DataFrame) -> None:
@@ -71,10 +98,10 @@ def plot_results(all_results_df: pd.DataFrame) -> None:
     librispeech_splits = all_results_df["librispeech_split"].unique()
     manipulations = ["zeroing", "permutation", "ablation"]
 
-    # Normalize the layer numbers to start from 0 and end at max layer for each model
-    all_results_df["norm_layer"] = all_results_df.groupby("modelname")[
-        "layer"
-    ].transform(lambda x: (x - x.min()) / (x.max() - x.min()))
+    # # Normalize the layer numbers to start from 0 and end at max layer for each model
+    # all_results_df["norm_layer"] = all_results_df.groupby("modelname")[
+    #     "layer"
+    # ].transform(lambda x: (x - x.min()) / (x.max() - x.min()))
 
     for probe, librispeech_split, manipulation in product(
         probes, librispeech_splits, manipulations
@@ -149,12 +176,21 @@ def plot_results(all_results_df: pd.DataFrame) -> None:
                 color="black",
                 linetype="dotted",
             )
-            + p9.ggtitle(
-                f"Probe: {probe}, Librispeech Split: {librispeech_split} Manipulation: {manipulation}"
+            + p9.theme(
+                figure_size=(10, 6),
+                dpi=300,
+                plot_caption=p9.element_text(
+                    ha="left", margin={"t": 1, "units": "lines"}
+                ),
             )
-            + p9.xlab("Layer")
-            + p9.ylab("Test Score")
-            + p9.theme(figure_size=(10, 6), dpi=300)
+            + p9.scale_color_discrete(name="Manipulated Feature Group")
+            + p9.labs(
+                x="Layer (from shallow to deep, normalized)",
+                y="Test Score (R²)",
+                title="Encoding Probe Performance Across Model Layers",
+                subtitle=f"Probe: {probe}, Librispeech Split: {librispeech_split} Manipulation: {manipulation}",
+                caption=caption,
+            )
         )
         figure.show()
 

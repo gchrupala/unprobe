bb3ba996848788392d475318def7015279fd371f
Gaofei Shen <g.shen@tilburguniversity.edu>
2025-10-29 15:01:30 +0100

    adding snellius dir and old plotting function and job script

--- job_preprocessing.sh
+++ job_preprocessing.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#SBATCH --error=logdir/log.preproc.%A_%a.err
-#SBATCH --output=logdir/log.preproc.%A_%a.out
+#SBATCH --error=logdir/log.preproc.%A.err
+#SBATCH --output=logdir/log.preproc.%A.out
 #SBATCH --job-name="preproc"
-#SBATCH --gres=gpu:A40:1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=01:00:00
 
 
 cd /home/gshen/work_dir/unprobe
 source .venv/bin/activate
 
 # uv run src/preprocessing.py --librispeech_split dev-clean --overwrite
-uv run src/preprocessing.py --librispeech_split train-clean-100 --overwrite
+srun python src/preprocessing.py --do_base_only --overwrite_base --overwrite_textgrid --librispeech_split train-clean-100
+srun python src/preprocessing.py --librispeech_split train-clean-100 --modelname "facebook/wav2vec2-base" --overwrite

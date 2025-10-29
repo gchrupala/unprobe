f75087523444d1ca3f46c06aa0fff883e08da696
Gaofei Shen <g.shen@tilburguniversity.edu>
2025-10-29 15:07:43 +0100

    updating dependency

--- job_preprocessing.sh
+++ job_preprocessing.sh
@@ -2,6 +2,8 @@
 
 #SBATCH --error=logdir/log.preproc.%A.err
 #SBATCH --output=logdir/log.preproc.%A.out
+#SBATCH --error=logdir/log.preproc.%A.err
+#SBATCH --output=logdir/log.preproc.%A.out
 #SBATCH --job-name="preproc"
 #SBATCH --gpus-per-node=1
 #SBATCH --time=01:00:00
@@ -10,6 +12,10 @@
 cd /home/gshen/work_dir/unprobe
 source .venv/bin/activate
 
-# uv run src/preprocessing.py --librispeech_split dev-clean --overwrite
-srun python src/preprocessing.py --do_base_only --overwrite_base --overwrite_textgrid --librispeech_split train-clean-100
-srun python src/preprocessing.py --librispeech_split train-clean-100 --modelname "facebook/wav2vec2-base" --overwrite
+export PATH="/home/gshen/.pixi/bin:/home/gshen/.local/bin:$PATH"
+source ~/.pixi/completions/bash/*
+
+
+# srun uv run --extra cu118 src/preprocessing.py --librispeech_split dev-clean --modelname "facebook/wav2vec2-base"  --overwrite
+srun python src/preprocessing.py --librispeech_split "train-clean-100" --do_base
+srun python src/preprocessing.py --librispeech_split train-clean-100 --do_transformer
